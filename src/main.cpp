ae915c758ff41c8dc5ba505a501a5b2c4ab117c0
Per Lennartsson <perlennartsson@outlook.com>
2025-03-09 19:53:15 +0100

    Update values and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,7 @@ int serial_enabled = 0;
 size_t refresh_count = 0;
 size_t scan_count = 0;
 bool shouldUpdate = true;
+#define DATA_TOKENS 3
 
 #if 1
 unsigned char BlackImage[EPD_ARRAY]; // Define canvas space
@@ -159,15 +160,11 @@ void loop()
         characteristic2.discover();
         characteristic3.discover();
         characteristic4.discover();
-        const char mydata[] = {1,2,3};
-        const char* mychardata = mydata;
-        characteristic4.write( (const uint8_t*) mychardata, strlen(mychardata) );
 
         const size_t CHARACTERISTIC_MAX_DATA_LEN = connection->getMtu() - 3;
         char buffer[1 * CHARACTERISTIC_MAX_DATA_LEN] = {0};
         char *current_pos = buffer;
 
-        //writeSerial(String(characteristic1.read8()));
         size_t bytes_received = characteristic1.read(current_pos, CHARACTERISTIC_MAX_DATA_LEN);
         current_pos += bytes_received;
         //bytes_received = characteristic2.read(current_pos, CHARACTERISTIC_MAX_DATA_LEN);
@@ -178,29 +175,25 @@ void loop()
         current_pos += bytes_received;*/
         memset(current_pos, 0, buffer + sizeof(buffer) - current_pos);
         Bluefruit.disconnect(connection->handle());
+
         String data = String(buffer);
-        // parseHomeAssistantData(String(data), haData);
         writeSerial(String("data: " + data));
+        char *tokens[DATA_TOKENS]; // Array to store token pointers
+        char *token;
+        int index = 0;
+    
+        // Use strtok to split the string
+        token = strtok(buffer, ",");
+        while (token != NULL && index < DATA_TOKENS) {
+            tokens[index] = token;  // Store pointer to token
+            index++;
+            token = strtok(NULL, ",");
+        }
 
-        /*if (haData.last_updated == "null")
-        {
-            writeSerial("Last updated is null. Trying again.");
-            delay(500);
-            startScan();
-            return;
-        }*/
-        // writeSerial("Last updated: " + String(haData.last_updated));
-        // writeSerial("Temperature inside: " + String(haData.temperature_inside));
 
-        // digitalWrite(D0, HIGH); // turn on display power
-        // display.init(115200, false, 2, true);
-        //  Partial refresh not working after full power off...
-        //  display.setPartialWindow(0, 0, display.width(), display.height());
-
-        writeDisplayData(buffer);
-
-        // hibernateDisplay();
-
+        if(token[0] == '1') {
+            writeDisplayData(tokens);
+        }
         refresh_count++;
         delay(TIME_REFRESH);
         startScan();
@@ -211,16 +204,6 @@ void loop()
         delay(TIME_RETRY_SCAN);
         startScan();
     }
-    // writeSerial("Doing something");
-    if (shouldUpdate)
-    {
-        EPD_HW_Init_GUI();                                         // GUI initialization.
-        Paint_Clear(WHITE);                                        // Clear canvas.
-        Paint_DrawString_EN(0, 45, "test", &Font20, WHITE, BLACK); // 14*20.
-        EPD_Display(BlackImage);                                   // Display GUI image.
-        EPD_DeepSleep();
-        shouldUpdate = false;
-    } // EPD_DeepSleep,Sleep instruction is necessary, please do not delete!!!
 }
 
 void startScan()
@@ -236,17 +219,6 @@ void startScan()
     Bluefruit.Scanner.start(0); // Scan timeout in 10 ms units
     delay(100);
 }
-
-/*void hibernateDisplay()
-{
-    writeSerial("jksfb");
-    // I don't know why these steps are needed, but there's no
-    // other chance to get below 30 uA
-    display.hibernate();
-    pinMode(D4, INPUT); // RST
-    SPI.end();
-    digitalWrite(D0, LOW);
-}*/
 
 void writeSerial(String message, bool newLine)
 {
@@ -299,138 +271,29 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
     writeSerial("Disconnected");
 }
 
-void printForecast(int offset_x, int offset_y, weather_icon icon, float temperature, String time)
-{
-    /*display.setFont(&GothamRounded_Book14pt8b);
-
-    display.setCursor(OFFSET_LEFT + offset_x, OFFSET_TOP + offset_y);
-    display.print(time);
-
-    display.drawBitmap(OFFSET_LEFT + offset_x + 15, OFFSET_TOP + offset_y + 8, icon, GLYPH_SIZE_WEATHER_SMALL, GLYPH_SIZE_WEATHER_SMALL, GxEPD_BLACK);
-
-    if (temperature > 9.99)
-    {
-      display.setCursor(OFFSET_LEFT + offset_x + 10, OFFSET_TOP + offset_y + 10 + GLYPH_SIZE_WEATHER_SMALL + 28);
-    }
-    else
-    {
-      display.setCursor(OFFSET_LEFT + offset_x + 15, OFFSET_TOP + offset_y + 10 + GLYPH_SIZE_WEATHER_SMALL + 28);
-    }
-    display.setFont(&GothamRounded_Bold14pt8b);
-    display.printf("%.0f°C", temperature);*/
-}
-
-void writeDisplayData(char text[])
+void writeDisplayData(char *tokens[DATA_TOKENS])
 {
     float batteryVoltage = getBatteryVoltage();
     char c[50]; //size of the number
     snprintf(c, 50, "%f", batteryVoltage);
     EPD_HW_Init_GUI();                                       // GUI initialization.
     Paint_Clear(WHITE);                                      // Clear canvas.
-    Paint_DrawString_EN(0, 45, text, &Font20, WHITE, BLACK); // 14*20.
-    Paint_DrawString_EN(10,80, c, &Font20, WHITE, BLACK);
+    //Draw Todays Date
+    Paint_DrawString_EN(0, 45, "Date: ", &Font20, WHITE, BLACK); 
+    Paint_DrawString_EN(80, 45, tokens[1], &Font20, WHITE, BLACK);
+    //Draw outdoor temp
+    Paint_DrawString_EN(0, 90, "Temp Ute: ", &Font12, WHITE, BLACK); 
+    Paint_DrawString_EN(100, 90, tokens[1], &Font12, WHITE, BLACK);
+    //Draw Battery
+    Paint_DrawString_EN(0, 250, "Batt: ", &Font12, WHITE, BLACK); 
+    Paint_DrawString_EN(40,250, c, &Font12, WHITE, BLACK);
     EPD_Display(BlackImage);                                 // Display GUI image.
     EPD_DeepSleep();
-    /*display.setRotation(1);
-
-    display.setTextColor(GxEPD_BLACK);
-    int16_t tbx, tby;
-    uint16_t tbw, tbh;
-    display.setFont(&GothamRounded_Bold32pt7b); // title
-    display.getTextBounds(title, 0, 0, &tbx, &tby, &tbw, &tbh);
-
-    display.firstPage();
-
-    do
-    {
-      // print Title
-      display.setCursor(((display.width() - tbw) / 2) - tbx, OFFSET_TOP + 80);
-      display.print(title);
-  /*
-      // print big weather icon and temperature
-      weather_icon weather_icon = get_weather_icon(haData.weather_forecast_now);
-      display.drawBitmap(OFFSET_LEFT + 10, OFFSET_TOP + 110, weather_icon, GLYPH_SIZE_WEATHER, GLYPH_SIZE_WEATHER, GxEPD_BLACK);
-      display.setCursor(OFFSET_LEFT + 110, OFFSET_TOP + 190);
-      display.setFont(&GothamRounded_Bold48pt8b);
-      display.printf("%.1f°C", haData.temperature_outside);
-      export LC_ALL=sv_SE.UTF-8
-      export LANG=sv_SE.UTF-8
-
-      // print humidity and wind
-      display.setFont(&GothamRounded_Bold14pt8b);
-      size_t weatherdetails_offset = 250;
-      display.drawBitmap(OFFSET_LEFT + 30, OFFSET_TOP + weatherdetails_offset - GLYPH_SIZE_WEATHER_SMALL / 2, weather_small_wind, GLYPH_SIZE_WEATHER_SMALL, GLYPH_SIZE_WEATHER_SMALL, GxEPD_BLACK);
-      display.setCursor(OFFSET_LEFT + 90, OFFSET_TOP + weatherdetails_offset + GLYPH_SIZE_WEATHER_SMALL / 4);
-      display.printf("%.1fkm/h", haData.wind_speed);
-
-      display.drawBitmap(OFFSET_LEFT + 230, OFFSET_TOP + weatherdetails_offset - GLYPH_SIZE_WEATHER_SMALL / 2, icon_humidity, GLYPH_SIZE_WEATHER_SMALL, GLYPH_SIZE_WEATHER_SMALL, GxEPD_BLACK);
-      display.setCursor(OFFSET_LEFT + 290, OFFSET_TOP + weatherdetails_offset + GLYPH_SIZE_WEATHER_SMALL / 4);
-      display.printf("%.1f%%", haData.humidity_outside);
-
-      // print forecasts
-      size_t forecast_offset_y = weatherdetails_offset + 60;
-      printForecast(30, forecast_offset_y, get_weather_icon(haData.weather_forecast_2h, true), haData.weather_forecast_2h_temp, haData.weather_forecast_2h_time);
-      printForecast(130, forecast_offset_y, get_weather_icon(haData.weather_forecast_4h, true), haData.weather_forecast_4h_temp, haData.weather_forecast_4h_time);
-      printForecast(230, forecast_offset_y, get_weather_icon(haData.weather_forecast_6h, true), haData.weather_forecast_6h_temp, haData.weather_forecast_6h_time);
-      printForecast(330, forecast_offset_y, get_weather_icon(haData.weather_forecast_8h, true), haData.weather_forecast_8h_temp, haData.weather_forecast_8h_time);
-
-      // living room temperature
-      display.drawBitmap(OFFSET_LEFT + 30, OFFSET_TOP + 430, icon_living_room, 80, 80, GxEPD_BLACK);
-      display.drawBitmap(OFFSET_LEFT + 120, OFFSET_TOP + 430, icon40_thermometer, 40, 40, GxEPD_BLACK);
-      display.drawBitmap(OFFSET_LEFT + 120, OFFSET_TOP + 470, icon40_humidity, 40, 40, GxEPD_BLACK);
-
-      display.setFont(&GothamRounded_Bold14pt8b);
-      display.setCursor(OFFSET_LEFT + 165, OFFSET_TOP + 460);
-      display.printf("%.1f°C", haData.temperature_inside);
-
-      display.setCursor(OFFSET_LEFT + 165, OFFSET_TOP + 500);
-      display.printf("%.1f%%", haData.humidity_inside);
-
-
-      // dog age
-      int y, M, d, h, m;
-      float s;
-      sscanf(haData.last_updated.c_str(), "%d-%d-%dT%d:%d:%f+00:00", &y, &M, &d, &h, &m, &s); // "2024-04-15T15:26:00.392326+00:00"
-
-      std::tm now_tm = {0, 0, 0, d, M - 1, y - 1900};
-      std::time_t now = std::mktime(&now_tm);
-      std::tm birthday_tm = {0, 0, 0, 25, 2, 2024 - 1900};
-      */
-    /* March 25, 2024 */
-
-    /*
-    std::time_t birthday = std::mktime(&birthday_tm);
-
-    int years_old = std::difftime(now, birthday) / (365.25 * 24 * 3600);
-    int months_old = (std::difftime(now, birthday) - (years_old * 365.25 * 24 * 3600)) / (30.44 * 24 * 3600);
-    int total_months_old = years_old * 12 + months_old;
-
-    display.drawBitmap(OFFSET_LEFT + 30, OFFSET_TOP + 550, image_dog, 80, 50, GxEPD_BLACK);
-    display.setCursor(OFFSET_LEFT + 120, OFFSET_TOP + 585);
-    display.printf("%d Monate", total_months_old);
-
-    display.setFont(&GothamRounded_Book14pt8b);
-
-    // battery level
-    float batteryVoltage = getBatteryVoltage();
-    display.setCursor(OFFSET_LEFT + 5, OFFSET_TOP + 670);
-    display.printf("%.1fV", batteryVoltage);
-
-    // last update
-    display.getTextBounds("STAND 11:11", 0, 0, &tbx, &tby, &tbw, &tbh);
-    display.setCursor(((display.width() - tbw) / 2) - tbx - 10, OFFSET_TOP + 670);
-    display.printf("STAND %s", haData.time.c_str());
-
-    // calibrate borders
-    // display.drawRect(OFFSET_LEFT, OFFSET_TOP, display.width() - OFFSET_LEFT - OFFSET_RIGHT, display.height() - OFFSET_TOP - OFFSET_BOTTOM, GxEPD_BLACK);
-
-} while (display.nextPage());*/
 }
 
 float getBatteryVoltage()
 {
     unsigned int adcCount = analogRead(PIN_VBAT);
     float adcVoltage = adcCount * VREF / ADC_MAX;
-    char output[50];
     return ((510e3 + 1000e3) / 510e3) * adcVoltage;
 }
